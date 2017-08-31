099a0350be36a2904ce8e1b2fd26df328dcce9f4
velascoDev <dracontium2@gmail.com>
2017-08-31 10:26:03 -0500

    cover bug fixes

--- public/js/cover.js
+++ public/js/cover.js
@@ -1,23 +1,32 @@
 function adjustBodyStyle() {
 	document.body.style.transition = '';
 }
-function onMouseMove(event) {
-	const X_OFF = Math.floor(SCROLL_INDEX * event.clientX) - MARGIN;
-	const Y_OFF = Math.floor(SCROLL_INDEX * event.clientY) - MARGIN;
-	document.body.style.backgroundPosition = `${X_OFF}px ${Y_OFF}px`;
+
+function onMouseMove(event, SCROLL_INDEX, MARGIN) {
+	function cb(event) {
+		const X_OFF = Math.floor(SCROLL_INDEX * event.clientX) - MARGIN;
+		const Y_OFF = Math.floor(SCROLL_INDEX * event.clientY) - MARGIN;
+		document.body.style.backgroundPosition = `${X_OFF}px ${Y_OFF}px`;
+	}
+	return cb;
 }
 
-function onMouseEnter(event) {
-	const BODY_STYLE_DELAY = 500;
-	document.body.style.transition = 'background .5s ease';
-	setTimeout(adjustBodyStyle, BODY_STYLE_DELAY);
+function onMouseEnter(event, BODY_STYLE_DELAY) {
+	function cb(event) {
+		document.body.style.transition = 'background .5s ease';
+		setTimeout(adjustBodyStyle, BODY_STYLE_DELAY);
+	}
+	return cb;
 }
 
 function cover() {
 	const cover = document.getElementById('cover');
 	const MARGIN = 200;
 	const SCROLL_INDEX = .05;
-	cover.addEventListener('mousemove', onMouseMove);
-	cover.addEventListener('mouseenter', onMouseEnter);
+	const BODY_STYLE_DELAY = 500;
+	cover.addEventListener('mousemove', onMouseMove(event, SCROLL_INDEX, MARGIN));
+	cover.addEventListener('mouseenter', onMouseEnter(event, BODY_STYLE_DELAY));
 }
+
+export default cover;
 
--- public/js/index.js
+++ public/js/index.js
@@ -1,3 +1,9 @@
 import Rellax from 'rellax';
 import SmoothScroll from 'smooth-scroll';
-import './cover';
+import cover from './cover';
+
+document.addEventListener("DOMContentLoaded", (event) => { 
+	const rellax = new Rellax('.rellax');
+	const scroll = new SmoothScroll('a[href*="#"]');
+	cover();
+});
